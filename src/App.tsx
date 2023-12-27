67d99d15c9246e569d0788fc21ad7b642011581c
Ilya <lord-ilja@yandex.ru>
2023-12-27 19:13:23 +0300

    add fonts, styles. Add libraries + settings(clsx,generate components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.module.scss'
+import { Button } from 'components/UI/Button'
+import './App.css'
 
 const App = () => {
-  return <></>
+  return (
+    <div className="app">
+      <h1>App</h1>
+      <Button>Click Me!</Button>
+      <Button appearance="big">Click Me!</Button>
+    </div>
+  )
 }
 
 export default App
