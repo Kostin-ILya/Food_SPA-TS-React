2db77e10a68ceb306eb388c42a02c119ed728d89
Ilya <lord-ilja@yandex.ru>
2024-01-07 00:22:12 +0300

    add React-router(v6) , change main styles,  delete App

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,29 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+import { MainLayout } from 'layouts/MainLayout'
+import { MenuPage } from 'pages/MenuPage'
+
+import 'styles/main.scss'
+import 'styles/reset.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <MenuPage />,
+      },
+      {
+        path: 'menu',
+        element: <MenuPage />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+)
