1d4fc3af7b34ee9a5f6f0f8231365d2e0d345fd0
Ilya <lord-ilja@yandex.ru>
2024-01-13 00:31:19 +0300

    change router (createRoutesFromElements)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom'
 
 import { MainLayout } from 'layouts/MainLayout'
 import { MenuPage } from 'pages/MenuPage'
@@ -7,22 +12,34 @@ import { MenuPage } from 'pages/MenuPage'
 import 'styles/main.scss'
 import 'styles/reset.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <MenuPage />,
-      },
-      {
-        path: 'menu',
-        element: <MenuPage />,
-      },
-    ],
-  },
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <MainLayout />,
+//     children: [
+//       {
+//         index: true,
+//         element: <MenuPage />,
+//       },
+//       {
+//         path: 'menu',
+//         element: <MenuPage />,
+//       },
+//     ],
+//   },
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<MenuPage />} />
+        <Route path="menu" element={<MenuPage />} />
+      </Route>
+      <Route path="*" element={<h1>Page not found</h1>} />
+    </>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router} />
